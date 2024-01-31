960d2ffc6ed4223b5dee415a9c57f13e1ae85f25
aharaldson <a.haraldson@gmail.com>
2024-01-30 17:34:43 -0700

    setup

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -16,12 +16,12 @@ export default function App() {
     <main className="App">
       { user ?
           <>
-            <NavBar user={user} setUser={setUser}/>
             <Routes>
               {/* Route components in here */}
               <Route path="/orders/new" element={<NewOrderPage />} />
               <Route path="/orders" element={<OrderHistoryPage />} />
             </Routes>
+            <NavBar user={user} setUser={setUser}/>
           </>
           :
           <AuthPage setUser={setUser} />
